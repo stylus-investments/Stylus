deea1161214570699c4713e88cda65e6dc4ada12
Arnold <arnoldzkie22@gmail.com>
2024-04-18 15:36:21 +0800

    fixed header

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -6,7 +6,7 @@ import { ToggleTheme } from '../ui/toggle-theme'
 
 const Header = () => {
   return (
-    <div className='flex fixed top-0 left-0 w-screen h-20 padding items-center justify-between'>
+    <div className='flex fixed top-0 left-0 w-screen h-20 padding items-center justify-between border-b'>
       <div className='flex items-center gap-4'>
         <Image src={'/logo.png'} alt='logo' width={50} height={20} className='w-auto h-auto' />
         <h1 className='text-3xl font-black'>GrowPoint</h1>
