4608e346bfa15b88bbd7dab6f6a14f99cb6ab72a
Arnold <arnoldzkie22@gmail.com>
2024-04-27 11:43:51 +0800

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async headers() {
-        return [
-            {
-                source: "/api/:path*",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "https://www.growpoint.app" },
-                    { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Authorization, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
-                ]
-            }
-        ]
-    }
-};
+const nextConfig = {};
 
 export default nextConfig;
