d1b56836392643ec7a834d14680a5eb7ab8e2a45
Arnold <arnoldzkie22@gmail.com>
2024-04-27 11:39:25 +0800

    gana naunta

--- next.config.mjs
+++ next.config.mjs
@@ -6,9 +6,9 @@ const nextConfig = {
                 source: "/api/trpc/:path*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" },
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                    { key: "Access-Control-Allow-Origin", value: "https://www.growpoint.app" },
+                    { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Authorization, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
                 ]
             }
         ]
