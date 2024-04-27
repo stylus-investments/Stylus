28e46012fd659c95e323c38943c533a254eb5c9a
Arnold <arnoldzkie22@gmail.com>
2024-04-27 11:39:38 +0800

    gana naunta

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     async headers() {
         return [
             {
-                source: "/api/trpc/:path*",
+                source: "/api/:path*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "https://www.growpoint.app" },
