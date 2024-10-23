19574b5f5814fa4642e5fbcb6b672837d5891920
Arnold <arnoldzkie22@gmail.com>
2024-10-23 22:35:34 +0800

    added more skeleton

--- src/components/dashboard/wallet/assets-data.tsx
+++ src/components/dashboard/wallet/assets-data.tsx
@@ -37,72 +37,75 @@ const AssetsData = () => {
 
   const smallScreen = (
     <div className='flex flex-col w-full md:hidden'>
-      {assets && assets.length > 0 ? assets.map((asset, i) => (
-        <Link href={`/dashboard/wallet/assets/${asset?.address}`} className='flex hover:bg-muted  items-center justify-between px-3 py-4 border-b w-full' key={i}>
-          <div className='flex iems-start gap-2.5'>
-            <Image src={returnAssetIcon(asset?.symbol)} width={20} height={20} alt={asset?.name || ""} className='rounded-full max-h-[20px] max-w-[20px]' />
-            <div className='flex flex-col gap-3'>
-              <Label className='text-lg -mt-1.5'>{asset?.symbol}</Label>
-              <div className='text-muted-foreground text-xs -mt-2'>{asset?.name}</div>
-              <div className='text-xs text-muted-foreground'>
-                24h change
+      {
+        assets && assets.length > 0 ? assets.map((asset, i) => (
+          <Link href={`/dashboard/wallet/assets/${asset?.address}`} className='flex hover:bg-muted  items-center justify-between px-3 py-4 border-b w-full' key={i}>
+            <div className='flex iems-start gap-2.5'>
+              <Image src={returnAssetIcon(asset?.symbol)} width={20} height={20} alt={asset?.name || ""} className='rounded-full max-h-[20px] max-w-[20px]' />
+              <div className='flex flex-col gap-3'>
+                <Label className='text-lg -mt-1.5'>{asset?.symbol}</Label>
+                <div className='text-muted-foreground text-xs -mt-2'>{asset?.name}</div>
+                <div className='text-xs text-muted-foreground'>
+                  24h change
+                </div>
+                <div className='text-xs'>
+                  Total Value
+                </div>
               </div>
-              <div className='text-xs'>
-                Total Value
+            </div>
+            <div className='flex flex-col gap-2.5 text-right'>
+              <Label className='text-base -mt-1.5'>
+                {!showBalance ? "******" :
+                  asset?.amount ? Number(asset?.amount).toLocaleString('en-US', { minimumFractionDigits: 6, maximumFractionDigits: 6 }) : ''
+                }
+              </Label>
+              <div className='text-muted-foreground text-xs -mt-2'>
+                {asset?.value_array.map((obj, i) => {
+                  if (obj.currency === currency) {
+                    // Find the matching currency object
+                    const matchingCurrency = availableCurrencies.find(currency => currency.currency === obj.currency);
+                    if (!showBalance) return "******"
+                    return (
+                      <div className='flex items-center w-full justify-end' key={i}>
+                        {matchingCurrency && (
+                          <FontAwesomeIcon icon={matchingCurrency.icon} width={15} height={15} />
+                        )}
+                        <div>
+                          {(Number(obj.amount)).toLocaleString('en-US', { minimumFractionDigits: 6, maximumFractionDigits: 6 })}
+                        </div>
+                      </div>
+                    );
+                  }
+                })}
+              </div>
+              <div className={`text-xs ${Number(asset?.change) > 0 ? "text-green-500" : "text-red-500"}`}>
+                {Number(asset?.change) > 0 ? "+" : ""}{asset?.change}%
+              </div>
+              <div className='text-md'>
+                {asset?.total_value_array.map((obj, i) => {
+                  if (obj.currency === currency) {
+                    // Find the matching currency object
+                    const matchingCurrency = availableCurrencies.find(currency => currency.currency === obj.currency);
+                    if (!showBalance) return "********"
+                    return (
+                      <div className='flex items-center w-full' key={i}>
+                        {matchingCurrency && (
+                          <FontAwesomeIcon icon={matchingCurrency.icon} width={15} height={15} />
+                        )}
+                        <div>
+                          {(Number(obj.amount)).toLocaleString('en-US', { minimumFractionDigits: 6, maximumFractionDigits: 6 })}
+                        </div>
+                      </div>
+                    );
+                  }
+                })}
               </div>
             </div>
-          </div>
-          <div className='flex flex-col gap-2.5 text-right'>
-            <Label className='text-base -mt-1.5'>
-              {!showBalance ? "******" :
-                asset?.amount ? Number(asset?.amount).toLocaleString('en-US', { minimumFractionDigits: 6, maximumFractionDigits: 6 }) : ''
-              }
-            </Label>
-            <div className='text-muted-foreground text-xs -mt-2'>
-              {asset?.value_array.map((obj, i) => {
-                if (obj.currency === currency) {
-                  // Find the matching currency object
-                  const matchingCurrency = availableCurrencies.find(currency => currency.currency === obj.currency);
-                  if (!showBalance) return "******"
-                  return (
-                    <div className='flex items-center w-full justify-end' key={i}>
-                      {matchingCurrency && (
-                        <FontAwesomeIcon icon={matchingCurrency.icon} width={15} height={15} />
-                      )}
-                      <div>
-                        {(Number(obj.amount)).toLocaleString('en-US', { minimumFractionDigits: 6, maximumFractionDigits: 6 })}
-                      </div>
-                    </div>
-                  );
-                }
-              })}
-            </div>
-            <div className={`text-xs ${Number(asset?.change) > 0 ? "text-green-500" : "text-red-500"}`}>
-              {Number(asset?.change) > 0 ? "+" : ""}{asset?.change}%
-            </div>
-            <div className='text-md'>
-              {asset?.total_value_array.map((obj, i) => {
-                if (obj.currency === currency) {
-                  // Find the matching currency object
-                  const matchingCurrency = availableCurrencies.find(currency => currency.currency === obj.currency);
-                  if (!showBalance) return "********"
-                  return (
-                    <div className='flex items-center w-full' key={i}>
-                      {matchingCurrency && (
-                        <FontAwesomeIcon icon={matchingCurrency.icon} width={15} height={15} />
-                      )}
-                      <div>
-                        {(Number(obj.amount)).toLocaleString('en-US', { minimumFractionDigits: 6, maximumFractionDigits: 6 })}
-                      </div>
-                    </div>
-                  );
-                }
-              })}
-            </div>
-          </div>
-        </Link>
-      )) :
-        <div className='flex justify-center pt-20'>No Data.</div>
+          </Link>
+        )) : assets && assets.length === 0 ?
+          <div className='flex justify-center pt-20'>No Data.</div>
+          :
+          <AssetSkeletonSMall />
       }
     </div>
   )
@@ -182,7 +185,7 @@ const AssetsData = () => {
                   <TableCell>No Data</TableCell>
                 </TableRow>
                 :
-                <AssetSkeleton />
+                <AssetSkeletonLarge />
           }
         </TableBody>
       </Table>
@@ -197,7 +200,35 @@ const AssetsData = () => {
   )
 }
 
-const AssetSkeleton = () => {
+const AssetSkeletonSMall = () => {
+  return [1, 2, 3, 4].map(item => (
+    <div className='flex items-center justify-between px-3 py-4 border-b w-full' key={item}>
+      <div className='flex iems-start gap-2.5'>
+        <Skeleton className='h-6 w-6 rounded-full' />
+        <div className='flex flex-col gap-3'>
+          <Skeleton className='w-20 h-4' />
+          <Skeleton className='w-28 h-5' />
+          <div className='text-xs text-muted-foreground'>
+            24h change
+          </div>
+          <div className='text-xs'>
+            Total Value
+          </div>
+        </div>
+      </div>
+      <div className='flex flex-col gap-2.5 text-right'>
+        <Skeleton className='w-24 h-5' />
+        <div className='text-muted-foreground text-xs -mt-2'>
+          <Skeleton className='w-24 h-4' />
+        </div>
+        <Skeleton className='w-24 h-3' />
+        <Skeleton className='w-24 h-3' />
+      </div>
+    </div>
+  ))
+}
+
+const AssetSkeletonLarge = () => {
 
   return [1, 2, 3, 4].map(item => (
     <TableRow key={item}>
